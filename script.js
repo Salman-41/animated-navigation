2918f59e0c40c45ac847af55c848a69a80d3ce24
salman-41 <salman.yz@outlook.com>
2023-11-04 21:28:06 +0500

    Cleaning the code

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
+// Get the menu bars and overlay elements
 const menuBars = document.getElementById("menu-bars");
 const overlay = document.getElementById("overlay");
-const nav1 = document.getElementById("nav-1");
-const nav2 = document.getElementById("nav-2");
-const nav3 = document.getElementById("nav-3");
-const nav4 = document.getElementById("nav-4");
-const nav5 = document.getElementById("nav-5");
+
+// Get all navigation items
+const navItems = Array.from({ length: 5 }, (_, i) =>
+  document.getElementById(`nav-${i + 1}`)
+);
+
+// Define the slide-in and slide-out classes for each navigation item
+const slideInClasses = Array.from({ length: 5 }, (_, i) => `slide-in-${i + 1}`);
+const slideOutClasses = Array.from(
+  { length: 5 },
+  (_, i) => `slide-out-${i + 1}`
+);
 
 function toggleNav() {
-  // toggle: menu Bars Open/Closed
+  // Toggle the change class on the menu bars
   menuBars.classList.toggle("change");
-  //   toggle: menu Active
+
+  // Toggle the overlay-active class on the overlay
   overlay.classList.toggle("overlay-active");
-  if (overlay.classList.contains("overlay-active")) {
-    // Animate In - Overlay
-    overlay.classList.remove("overlay-slide-left");
-    overlay.classList.add("overlay-slide-right");
-    // Animate IN - Nav Items
-    nav1.classList.remove("slide-out-1");
-    nav1.classList.add("slide-in-1");
-    nav2.classList.remove("slide-out-2");
-    nav2.classList.add("slide-in-2");
-    nav3.classList.remove("slide-out-3");
-    nav3.classList.add("slide-in-3");
-    nav4.classList.remove("slide-out-4");
-    nav4.classList.add("slide-in-4");
-    nav5.classList.remove("slide-out-5");
-    nav5.classList.add("slide-in-5");
-  } else {
-    //Animate Out - Overlay
-    overlay.classList.remove("overlay-slide-right");
-    overlay.classList.add("overlay-slide-left");
-    // Animate Out - Nav Items
-    nav1.classList.remove("slide-in-1");
-    nav1.classList.add("slide-out-1");
-    nav2.classList.remove("slide-in-2");
-    nav2.classList.add("slide-out-2");
-    nav3.classList.remove("slide-in-3");
-    nav3.classList.add("slide-out-3");
-    nav4.classList.remove("slide-in-4");
-    nav4.classList.add("slide-out-4");
-    nav5.classList.remove("slide-in-5");
-    nav5.classList.add("slide-out-5");
-  }
+
+  // Check if the overlay is active
+  const isOverlayActive = overlay.classList.contains("overlay-active");
+
+  // Remove the slide-left or slide-right class from the overlay, depending on whether it's active
+  overlay.classList.remove(
+    isOverlayActive ? "overlay-slide-left" : "overlay-slide-right"
+  );
+
+  // Add the slide-right or slide-left class to the overlay, depending on whether it's active
+  overlay.classList.add(
+    isOverlayActive ? "overlay-slide-right" : "overlay-slide-left"
+  );
+
+  // For each navigation item
+  navItems.forEach((navItem, i) => {
+    // Remove the slide-out or slide-in class, depending on whether the overlay is active
+    navItem.classList.remove(
+      isOverlayActive ? slideOutClasses[i] : slideInClasses[i]
+    );
+
+    // Add the slide-in or slide-out class, depending on whether the overlay is active
+    navItem.classList.add(
+      isOverlayActive ? slideInClasses[i] : slideOutClasses[i]
+    );
+  });
 }
 
-// Event Listeners
+// Add a click event listener to the menu bars that toggles the navigation
 menuBars.addEventListener("click", toggleNav);
-nav1.addEventListener("click", toggleNav);
-nav2.addEventListener("click", toggleNav);
-nav3.addEventListener("click", toggleNav);
-nav4.addEventListener("click", toggleNav);
-nav5.addEventListener("click", toggleNav);
+
+// Add a click event listener to each navigation item that toggles the navigation
+navItems.forEach((navItem) => navItem.addEventListener("click", toggleNav));
